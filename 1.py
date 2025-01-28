56a3132e18574be809058d2c0dba65cde5087913
Paugomilabarber <pgomila20959@iesjoanramis.org>
2025-01-28 08:47:36 +0100

    1.

---
+++ 1.py
@@ -0,0 +1,3 @@
+"""Crear una funció que compti la longitud de cada paraula de una cadena de caràcters que li passem. 
+Utilitzar map. Ex: def lenp(frase): -- retorni una llista amb la longitud de cada paraula de la frase.
+"""
