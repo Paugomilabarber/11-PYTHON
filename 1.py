8129c33b9d95f584357a7ffbd8823866d9948284
Paugomilabarber <pgomila20959@iesjoanramis.org>
2025-01-28 09:51:41 +0100

    1.2

--- 1.py
+++ 1.py
@@ -1,3 +1,10 @@
 """Crear una funció que compti la longitud de cada paraula de una cadena de caràcters que li passem. 
 Utilitzar map. Ex: def lenp(frase): -- retorni una llista amb la longitud de cada paraula de la frase.
 """
+
+def lenp(frase):
+    return list(map(len, frase.split()))
+
+frase = input(" Dime algo: ")
+llargues = lenp(frase)
+print (llargues)
---
+++ 2.py
@@ -0,0 +1,20 @@
+"""Crear una funció que donada una llista de dígits ordenats, retorni el número corresponent. 
+Utilitzar reduce. Ex: [3, 4, 1, 5] correspòn al número 3415. Ex: def Passar_a_Numero(llista):
+ -- retorni el número que corresponen els dígits."""
+
+from functools import reduce
+
+def convertir_a_llista(cadena):
+    cadena = cadena.strip('[]')
+    elements = cadena.replace(",", " ").split()
+    return [int(x) for x in elements]
+
+
+def combinar_digits_base10(llista):
+    numero = reduce(lambda acc, x: acc * 10 + x, llista, 0)
+    return numero
+
+entrada_usuari = input("Introdueix una llista de dígits en base 10 : ")
+llista = convertir_a_llista(entrada_usuari)
+resultat = combinar_digits_base10(llista)
+print("Número en base 10:", resultat)
