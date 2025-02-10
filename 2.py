2c118aa72f2ed500f6870f37650bc29cb7daa121
Paugomilabarber <pgomila20959@iesjoanramis.org>
2025-02-10 13:37:40 +0100

    1.3

--- 2.py
+++ 2.py
@@ -1,6 +1,6 @@
 """Crear una funció que donada una llista de dígits ordenats, retorni el número corresponent. 
 Utilitzar reduce. Ex: [3, 4, 1, 5] correspòn al número 3415. Ex: def Passar_a_Numero(llista):
- -- retorni el número que corresponen els dígits."""
+ -retorni el número que corresponen els dígits."""
 
 from functools import reduce
 
