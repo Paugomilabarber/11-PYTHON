fab334474b82fc89d3c3db0879c15bce7c3f0406
Paugomilabarber <pgomila20959@iesjoanramis.org>
2025-02-11 08:12:35 +0100

    a

--- 2.py
+++ 2.py
@@ -5,19 +5,8 @@ Utilitzar reduce. Ex: [3, 4, 1, 5] correspòn al número 3415. Ex: def Passar_a_
 from functools import reduce
 
 def convertir_a_llista(cadena):
-    cadena = cadena.strip('[]')
-    elements = cadena.replace(",", " ").split()
-    return [int(x) for x in elements]
-
-
-def combinar_digits_base10(llista):
-    numero = reduce(lambda acc, x: acc * 10 + x, llista, 0)
-    return numero
-
-entrada_usuari = input("Introdueix una llista de dígits en base 10 : ")
-llista = convertir_a_llista(entrada_usuari)
-resultat = combinar_digits_base10(llista)
-print("Número en base 10:", resultat)
-llista_digitos = list(map(int, entrada.split()))
-numero = passar_a_numero(llista_digitos)
-print("El número formado es:", numero)
+    l = []
+    a = input("Dime una llista de numeros: ")
+    l.append(a)
+    lista = a.split(",")
+    
