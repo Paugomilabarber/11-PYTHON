cfbaabea8d836df9cab7954db4312d0471a9a8dc
Paugomilabarber <pgomila20959@iesjoanramis.org>
2025-02-12 13:44:24 +0100

    siuuu

--- 3.py
+++ 3.py
@@ -2,4 +2,14 @@
 #que comencen per la lletra donada. Utilitzar filter. Ex: [“maria”, “manta”, “peu”, “mà”] i 
 #li deim que ens retorni totes les que comencen per ‘p’, en retorni [‘peu’].
 
+def filtra_paraules():
+    paraules = input("Introdueix les paraules de la llista separades per comes: ").split(','' ')
+    lletra = input("Introdueix la lletra: ")
+    if not lletra:
+        print("No s'ha introduït cap lletra valida.")
+        return
+    primera = lletra[0]
+    resultat = list(filter(lambda paraula: paraula.startswith(primera), paraules))
+    print("Resultat: ", resultat)
 
+filtra_paraules()
--- 4.py
+++ 4.py
@@ -1,3 +1,13 @@
 #Crear una funció que donades dues llistes, les concateni amb un connector. 
 #Utilitzar zip. Ex: [‘sub’,’supra’] i [‘campió’ ‘campiona’] i el connector ‘-’, retorni [‘sub-campió’][‘supra-campiona’].
 
+def llista():
+    l = input("Introdueix les paraules de la primera llista separades per comes: ").split(',')
+    k = input("Introdueix les paraules de la segona llista separades per comes: ").split(',')
+    e = zip(l, k)
+    j = zip(l, k)
+
+    for e,j in zip(l,k):
+        print("Resultat: {}-{}".format(e,j))
+
+llista()
