b5bb17cb9d51ebf5de1790b27534b7d10dc68a85
Paugomilabarber <pgomila20959@iesjoanramis.org>
2025-02-12 12:41:12 +0100

    a

--- 4.py
+++ 4.py
@@ -1,4 +1,3 @@
-"""Crear una funció que donades dues llistes, les concateni amb un connector. 
-Utilitzar zip. Ex: [‘sub’,’supra’] i [‘campió’ ‘campiona’] i el connector ‘-’, retorni [‘sub-campió’][‘supra-campiona’].
-"""
+#Crear una funció que donades dues llistes, les concateni amb un connector. 
+#Utilitzar zip. Ex: [‘sub’,’supra’] i [‘campió’ ‘campiona’] i el connector ‘-’, retorni [‘sub-campió’][‘supra-campiona’].
 
