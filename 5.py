3e0e1a784894725852c9ffe145c84e6a0bc1e969
Paugomilabarber <pgomila20959@iesjoanramis.org>
2025-02-14 11:18:33 +0100

    5

---
+++ 5.py
@@ -0,0 +1,4 @@
+#Crear una funció que donada una llista, retorni un diccionari que tingui com a clau: 
+#els valors de la llista i com a valor el seu índex dins la llista. Utilitzar enumerate.
+#Ex: (‘casa’,’cotxe’,’cadira’,’taula’) retorni {‘casa’:0, ‘cotxe’:1, ‘cadira’:2, ‘taula’:3}.
+
